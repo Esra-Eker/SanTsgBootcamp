6d71f72a5171ed8a849a02083c7447a4a2bb620f
Esra <elifesraeker@gmail.com>
2022-06-02 13:41:57 +0300

    Proje oluşturuldu

---
+++ Bootcamp.Domain/Users/User.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Bootcamp.Domain.Users
+{
+    public class User
+    {
+        [Key]
+        public int Id { get; set; }
+        [Required]
+        public string UserName { get; set; }
+        [Required]
+        public int Password { get; set; }
+        public string EmailAddress { get; set; }
+        public DateTime CreateDate { get; set; }
+    }
+}
---
+++ Bootcamp.Web/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace Bootcamp.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
